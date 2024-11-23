c968c0f4a7cfe7ed5990e6ca205e3b043f2d7435
Simone Guzzi <simone.guzzi@sib.swiss>
2024-11-23 20:25:21 +0100

    feat(linkml/attributes): drop unnecessary information

--- libs/linkml/src/lib/entities.ts
+++ libs/linkml/src/lib/entities.ts
@@ -1,6 +1,10 @@
 import { Attribute } from '@neo4j-arrows/model';
 import { toAttributeName } from './naming';
-import { CollectionType, Attribute as LinkMLAttribute } from './types';
+import {
+  BasicType,
+  CollectionType,
+  Attribute as LinkMLAttribute,
+} from './types';
 
 export const propertiesToAttributes = (
   attributes: Record<string, Attribute>
@@ -12,14 +16,26 @@ export const propertiesToAttributes = (
     ) => ({
       ...attributes,
       [toAttributeName(key)]: {
-        description,
-        multivalued:
-          !!collectionType &&
-          collectionType !== '' &&
-          [CollectionType.LIST, CollectionType.SET].includes(collectionType),
-        unique_values: collectionType === CollectionType.SET,
-        required: required ?? false,
-        range,
+        ...{
+          description,
+          required,
+        },
+        ...([...Object.values(BasicType)].includes(range as BasicType)
+          ? { range }
+          : {}),
+        ...(collectionType
+          ? {
+              multivalued:
+                !!collectionType &&
+                collectionType !== '' &&
+                [CollectionType.LIST, CollectionType.SET].includes(
+                  collectionType
+                ),
+            }
+          : {}),
+        ...(collectionType === CollectionType.SET
+          ? { unique_values: true }
+          : {}),
       },
     }),
     {}
