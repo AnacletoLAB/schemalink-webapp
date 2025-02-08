6a0c76f68e2b0ee6c273c1458ee9aea03d23a4b1
simone.guzzi <simone.guzzi@sib.swiss>
2025-02-08 12:42:42 +0100

    feat(linkml/toYaml): add newlines to improve readability

--- libs/linkml/src/index.ts
+++ libs/linkml/src/index.ts
@@ -293,5 +293,9 @@ export const toYaml = (linkML: LinkML): string => {
         'unique_values: true',
         '# unique_values: true --> not supported yet'
       )
+      // Add a newline before each class name (except the first one).
+      .replace(/(\n\s+[^\n]*)(\n\s+[A-Z][a-z])/g, '$1\n$2')
+      // Add a newline before each first-level object
+      .replace(/(\n\S)/g, '\n$1')
   );
 };
